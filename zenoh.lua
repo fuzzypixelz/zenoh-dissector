fb663c513e9ec4476c3824cf58d2b1444206eb6d
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2021-09-27 12:46:48 +0200

    Fix header

--- zenoh.lua
+++ zenoh.lua
@@ -1,18 +1,18 @@
-//
-// Copyright (c) 2021 Carlos Guimarães
-//
-// This program and the accompanying materials are made available under the
-// terms of the Eclipse Public License 2.0 which is available at
-// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-// which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
-// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-//
-// Contributors:
-//   Carlos Guimarães, <carlos.em.guimaraes@gmail.com>
-//   Luca Cominardi, <luca.cominardi@gmail.com>
-//   Angelo Corsaro, <angelo@icorsaro.net>
-//
+--
+-- Copyright (c) 2021 Carlos Guimarães
+--
+-- This program and the accompanying materials are made available under the
+-- terms of the Eclipse Public License 2.0 which is available at
+-- http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+-- which is available at https://www.apache.org/licenses/LICENSE-2.0.
+--
+-- SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+--
+-- Contributors:
+--   Carlos Guimarães, <carlos.em.guimaraes@gmail.com>
+--   Luca Cominardi, <luca.cominardi@gmail.com>
+--   Angelo Corsaro, <angelo@icorsaro.net>
+--
 
 --- DISSECTOR INFO & FIELDS ---
 local proto_zenoh_tcp = Proto("zenoh-tcp", "Zenoh Protocol over TCP")
